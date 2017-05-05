856b280fd3c53db98a38909149b7b4bdbf1621f7
Alan Bradley <alan@skoon.it>
2017-05-05 07:47:42 +0100

    travis deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+REPO=$(git config remote.origin.url)
+
+if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+	exit 1
+else
+	if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+		echo "Travis doesn't deploy from pull requests"
+		exit 0
+	else
+		REPO=${REPO/git:\/\/github.com\//git@github.com:}
+		REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+		chmod 600 $SSH_KEY
+		eval `ssh-agent -s`
+		ssh-add $SSH_KEY
+		git config --global user.name "$GIT_NAME"
+		git config --global user.email "$GIT_EMAIL"
+	fi
+fi
+
+REPO_NAME=$(basename $REPO)
+TARGET_DIR=$(mktemp -d /tmp/$REPO_NAME.XXXX)
+REV=$(git rev-parse HEAD)
+git clone --branch ${TARGET_BRANCH} ${REPO} ${TARGET_DIR}
+rsync -rt --delete --exclude=".git" --exclude=".travis.yml" $SOURCE_DIR/ $TARGET_DIR/
+cd $TARGET_DIR
+git add -A .
+git commit --allow-empty -m "Built from commit $REV"
+git push $REPO $TARGET_BRANCH
+
+git config --global user.name "$GIT_NAME"
+git config --global user.email "$GIT_EMAIL"
